4479532c74ec9eec97d943eb00df64942b83a300
mackpounds <igiliegbepatrick@gmail.com>
2025-05-18 17:43:16 +0100

    setup2

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector('.btn');
+    btn .addEventListener('click', () => {
+        const result = document.querySelector('.conf');
+        result.innerText = `Your WebApp is ready for Action.`;
+    });
