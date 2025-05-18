d6b3dbc36463467a6f1f4c1deabcb94799923037
mackpounds <igiliegbepatrick@gmail.com>
2025-05-18 21:55:48 +0100

    first-layout

--- assets/js/script.js
+++
@@ -1,5 +0,0 @@
-const btn = document.querySelector('.btn');
-    btn .addEventListener('click', () => {
-        const result = document.querySelector('.conf');
-        result.innerText = `Your WebApp is ready for Action.`;
-    });
